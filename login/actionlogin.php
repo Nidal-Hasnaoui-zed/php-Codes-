7ffea94576b875227b40e48a295f95f7b1384c1c
Hairo <hasnaouinidal308@gmail.com>
2025-08-05 23:12:22 +0100

    making the fukcing list good !

--- login/actionlogin.php
+++ login/actionlogin.php
@@ -10,7 +10,18 @@
         $pwd = test_input($_POST['pwd']);
 
         if(!empty($name) && !empty($pwd)){
-
+            include('../cnx.php');
+            $req = 'select * from Surveillant where name=? and pwd=?';
+            $stmt= $conn->prepare($req); 
+            $stmt->execute([$name,md5($pwd)]); 
+            if($stmt->rowCount() == 1){
+                session_start(); 
+                $
+            }else{
+                // user name or password is incorrect !
+                header('location:login.php?p=3'); 
+                exit();
+                }
         }else{
             // you leav an empty input !
             header('location:login.php?p=2'); 
